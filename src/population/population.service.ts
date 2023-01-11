a4635989a873460dcbe12e2a46cfda9aebcd4a3e
baeksbs3 <baeksbs3@gmail.com>
2023-01-11 11:24:27 +0900

    HttpException added

--- src/population/population.service.ts
+++ src/population/population.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { CreatePopulationDto } from './dto/create-population.dto';
-import { UpdatePopulationDto } from './dto/update-population.dto';
 import convert from 'xml-js';
-import removeJsonTextAttribute from 'src/common/functions/xml.value.converter';
+import removeJsonTextAttribute from '../common/functions/xml.value.converter';
+import { HttpException } from '@nestjs/common/exceptions';
+// import { CreatePopulationDto } from './dto/create-population.dto';
+// import { UpdatePopulationDto } from './dto/update-population.dto';
+
 @Injectable()
 export class PopulationService {
   constructor(private readonly httpService: HttpService) {}
 
   async findAll(placeId: string) {
-    const url = `http://openapi.seoul.go.kr:8088/${process.env.API_SECRET_KEY}/xml/citydata/1/5/${placeId}`;
+    const url = `http://openapi.seoul.go.kr:8088/${process.env.POP_API_KEY}/xml/citydata/1/5/${placeId}`;
 
     const rawData = await this.httpService.get(encodeURI(url)).toPromise();
     const data = convert.xml2json(rawData.data, {
@@ -17,11 +19,10 @@ export class PopulationService {
       spaces: 2,
       textFn: removeJsonTextAttribute,
     });
-
     const result = JSON.parse(data);
-    const populationData = result['SeoulRtd.citydata'].CITYDATA.LIVE_PPLTN_STTS;
-
-    return populationData;
+    if (!result['SeoulRtd.citydata'])
+      throw new HttpException('wrong place name', 404);
+    else return result['SeoulRtd.citydata'].CITYDATA.LIVE_PPLTN_STTS;
   }
 }
 
