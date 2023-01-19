fa0b108fa85fb00afa8c6d5399f78285e45305b6
chocheolhee <whcjfgml92@naver.com>
2023-01-19 20:22:18 +0900

    busApi_saveBusData redis added

--- src/bus/bus.service.ts
+++ src/bus/bus.service.ts
@@ -1,3 +1,4 @@
+import areaList from 'src/common/area-list';
 import { PlaceIdRequestDto } from './dto/placeId-request.dto';
 import removeJsonTextAttribute from 'src/common/functions/xml.value.converter';
 import {
@@ -18,78 +19,54 @@ export class BusService {
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
+  async saveBusData() {
+    for (const area of areaList) {
+      const url = `http://openapi.seoul.go.kr:8088/${process.env.BUS_API_SECRET_KEY}/xml/citydata/1/50/${area['AREA_NM']}`;
+      const stream = this.httpService.get(encodeURI(url));
+      const rawData = await lastValueFrom(stream);
+
+      const data = JSON.parse(
+        convert.xml2json(rawData.data, {
+          compact: true,
+          spaces: 2,
+          textFn: removeJsonTextAttribute,
+        }),
+      )['SeoulRtd.citydata'].CITYDATA.BUS_STN_STTS.BUS_STN_STTS;
+
+      const busData = {
+        AREA_NM: area['AREA_NM'],
+        ...data,
+      };
+
+      await this.cacheManager.set(
+        `BUS_${area['AREA_NM']}`,
+        JSON.stringify(busData),
+      );
+    }
+  }
+
   async findAll(placeId: PlaceIdRequestDto) {
-    const test = JSON.parse(await this.cacheManager.get('test'));
-    if (test) {
-      const busData =
-        test['SeoulRtd.citydata'].CITYDATA.BUS_STN_STTS.BUS_STN_STTS;
+    const data = JSON.parse(await this.cacheManager.get(`BUS_${placeId}`));
 
-      for (const data of busData) {
-        delete data.BUS_DETAIL;
-      }
-
-      return busData;
+    if (!data) {
+      throw new HttpException('null busData', 404);
     }
 
-    const apiUrl = `http://openapi.seoul.go.kr:8088/${process.env.BUS_API_SECRET_KEY}/xml/citydata/1/2/${placeId}`;
-
-    const stream = this.httpService.get(encodeURI(apiUrl));
-    const result = await lastValueFrom(stream);
-
-    const data = convert.xml2json(result.data, {
-      compact: true,
-      spaces: 4,
-      textFn: removeJsonTextAttribute,
-    });
-    const dataToJson = JSON.parse(data);
-    await this.cacheManager.set('test', JSON.stringify(dataToJson));
-
-    if (!dataToJson['SeoulRtd.citydata'])
-      throw new HttpException('wrong place name', 404);
-
-    const busData =
-      dataToJson['SeoulRtd.citydata'].CITYDATA.BUS_STN_STTS.BUS_STN_STTS;
-
-    for (const data of busData) {
-      delete data.BUS_DETAIL;
+    for (const busData of data) {
+      delete busData.BUS_DETAIL;
     }
 
-    return busData;
+    return data;
   }
 
   async findOne(placeId: PlaceIdRequestDto, busId: number) {
-    const test = JSON.parse(await this.cacheManager.get('test'));
-    if (test) {
-      const busData =
-        test['SeoulRtd.citydata'].CITYDATA.BUS_STN_STTS.BUS_STN_STTS;
+    const data = JSON.parse(await this.cacheManager.get(`BUS_${placeId}`));
 
-      const resultData = busData.find((obj: any) => obj.BUS_STN_ID === busId);
-
-      if (!resultData) throw new HttpException('wrong busId', 404);
-
-      return resultData;
+    if (!data) {
+      throw new HttpException('null busData', 404);
     }
 
-    const apiUrl = `http://openapi.seoul.go.kr:8088/${process.env.BUS_API_SECRET_KEY}/xml/citydata/1/2/${placeId}`;
-
-    const stream = this.httpService.get(encodeURI(apiUrl));
-    const result = await lastValueFrom(stream);
-
-    const data = convert.xml2json(result.data, {
-      compact: true,
-      spaces: 4,
-      textFn: removeJsonTextAttribute,
-    });
-    const dataToJson = JSON.parse(data);
-    await this.cacheManager.set('test', JSON.stringify(dataToJson));
-
-    if (!dataToJson['SeoulRtd.citydata'])
-      throw new HttpException('wrong place name', 404);
-
-    const busData =
-      dataToJson['SeoulRtd.citydata'].CITYDATA.BUS_STN_STTS.BUS_STN_STTS;
-
-    const resultData = busData.find((obj: any) => obj.BUS_STN_ID === busId);
+    const resultData = data.find((obj: any) => obj.BUS_STN_ID === busId);
 
     if (!resultData) throw new HttpException('wrong busId', 404);
 
