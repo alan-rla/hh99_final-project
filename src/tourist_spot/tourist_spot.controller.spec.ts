1dcc598123d8b9dea18f74c6571416aa7af0599e
baeksbs3 <baeksbs3@gmail.com>
2023-02-04 01:07:57 +0900

    testcode_tourist_spot_service

---
+++ src/tourist_spot/tourist_spot.controller.spec.ts
@@ -0,0 +1,22 @@
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { ToursimController } from './tourist_spot.controller';
+// import { TourismService } from './tourist_spot.service';
+
+// describe('TourismController', () => {
+//   let controller: ToursimController;
+
+//   beforeEach(async () => {
+//     const module: TestingModule = await Test.createTestingModule({
+//       controllers: [ToursimController],
+//       providers: [TourismService],
+//     }).compile();
+
+//     controller = module.get<ToursimController>(ToursimController);
+//   });
+
+//   it('이 테스트의 결과는 data를 리턴해야한다 data는 배열안에 지역구의 모든 관광지정보가 출력된다', () => {
+//    const STATE_NM = '마포구'
+//     controller.findOneStateTourism(STATE_NM)
+//     expect(controller).toBeDefined();
+//   });
+// });
